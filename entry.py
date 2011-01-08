aed04331e82f66862ddf8fbe1aa8c7d052d15819
Enzo Yang <yzb2002@gmail.com>
2011-01-08 16:45:36 +0800

    the 'hello' router completed.

--- entry.py
+++ entry.py
@@ -1,1 +1,12 @@
 import web
+
+urls = ('/','hello')
+
+app = web.application(urls,globals())
+
+class hello:
+    def GET(self):
+        return 'hello world'
+
+if __name__ == '__main__':
+    app.run();
